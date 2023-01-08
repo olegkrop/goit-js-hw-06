4f46fed136c37efa594ddaa05b5810d90f660b0f
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-01-08 20:19:04 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const loginEmail = event.currentTarget.elements.email.value;
+    const loginPassword = event.currentTarget.elements.password.value;
+
+    if (loginEmail === '' || loginPassword === '') {
+        window.alert("Всі поля мають бути заповнені!");
+    } else {
+        const loginFormData = { loginEmail, loginPassword };
+        console.log(loginFormData);
+        loginForm.reset();
+    };
+});
